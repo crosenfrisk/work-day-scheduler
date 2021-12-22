8194ef26fa3a5e0debff7a74e9edd176a3271098
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2021-12-22 11:57:25 -0600

    created column for hours and column for time block content; still need to make column for save icons

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+const d = new Date("December 22 2021");
+document.getElementById("currentDay").innerHTML = d;
+
+// moment().format('LL');
+// console.log(moment.format);
+
+
+// Which hours would you like your day to display? This will be your default setting!
+var hours = createItemEl('#hour');
+
