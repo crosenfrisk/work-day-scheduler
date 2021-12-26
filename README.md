4c833469ff840d13a0943748a0dbd9bbc409dfa7
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2021-12-25 18:34:34 -0600

    added comments for js actions

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # work-day-scheduler
+
+// USER STORY:
+// As an employee with a busy schedule
+// I want to add important events to a daily planner
+// so that I can manage my time effectively.
+
+// ACCEPTANCE CRITERIA:
+// GIVEN I am using a daily planner to create a schedule
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calender [done]
+
+// WHEN I scroll down
+// THEN I am presented with time blocks for THAT day [in-progress]
+
+// THEN each time block is color-coded to indicate whether it is in the past, present, or future.
+
+// WHEN I click into a time block, then I can enter an event
+
+// WHEN I click save button for that time block
+// THEN the text for that event is saved in local storage.
+
+// WHEN I refresh the page
+// THEN the saved event persists.
--- assets/js/script.js
+++ assets/js/script.js
@@ -3,45 +3,34 @@ var today = new Date();
 var date =  moment().format('dddd, MMMM Do, YYYY');
 document.getElementById("currentDay").innerHTML= date;
 
-var userInputEl = document.getElementsByClassName('time-block-content');
+// Create main section with hour blocks dynamically
 
-// var savedItem = createElement('#time-block-content');
+// Get input from user for time block
+var userInputEl = document.getElementById('textarea');
 
-var createTask = function () {
-    console.log('hello!');
-}
+// Add userInput to localStorage
+// var savedItem = JSON.stringify(userInput);
+// localStorage.setItem("userInput", savedItem);
+
+// var savedItem = createElement('textarea');
+// document.getElementById('textarea').addEventListener("click", modifyInput );
+// function modifyInput(){};
+
+// Display userInput (ongoing)
+// var displayInput =  function() {
+    //  var text = " ";
+
+    //  Retrieves items from local storage
+    //  var savedItem = localStorage.getItem("savedItem");
+
+    //  Parse string into object
+    // var savedItemsArray = JSON.parse(savedItem);
+    //      for (var i=0; i < savedItem.length; i++) {
+
+    // }
 
 
 
 
-// moment().format('LL');
-// console.log(moment.format);
-
-
-// Which hours would you like your day to display? This will be your default setting!
-// var hours = createItemEl('hour');
-
-// USER STORY:
-// As an employee with a busy schedule
-// I want to add important events to a daily planner
-// so that I can manage my time effectively.
-
-// ACCEPTANCE CRITERIA:
-// GIVEN I am using a daily planner to create a schedule
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calender
-
-// WHEN I scroll down
-// THEN I am presented with time blocks for THAT day.
-
-// THEN each time block is color-coded to indicate whether it is in the past, present, or future.
-
-// WHEN I click into a time block, then I can enter an event
-
-// WHEN I click save button for that time block
-// THEN the text for that event is saved in local storage.
-
-// WHEN I refresh the page
-// THEN the saved event persists.
 
 // userInputEl.addEventListener("click", createTask);
